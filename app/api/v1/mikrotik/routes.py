e872198f0f80436700710ac91a726e2ff9ffc78f
mdrakibhasanbd <mdrakibhasanbd369@gmail.com>
2023-09-01 04:10:57 +0600

    pymongo database error handle db file and mikritik/routes

--- app/api/v1/mikrotik/routes.py
+++ app/api/v1/mikrotik/routes.py
@@ -1,25 +1,32 @@
-import json
-
 from flask import Blueprint, jsonify, request
 from ..dbconfig.db import get_mongo_db
 from ..dbconfig.jsoni import JSONEncoder
+from pymongo.errors import ServerSelectionTimeoutError
+import json
 
 mikrotik_bp = Blueprint('mikrotik', __name__)
 
-db = get_mongo_db()
+
+# Remove the global db definition
+
 @mikrotik_bp.route('/items', methods=['GET'])
 def get_items():
-    # db.drop_collection('mikrotik')
+    try:
+        db = get_mongo_db()
 
+        # Perform operations on the database
+        mikrotikData = []
+        findItems = db['mikrotik'].find()
+        mikrotikData.extend(findItem for findItem in findItems)
 
-    # db['mikrotik'].insert_many(items)
-    mikrotikData = []
-    findItems = db['mikrotik'].find()
-    mikrotikData.extend(findItem for findItem in findItems)
-
-
-
-    return json.dumps(mikrotikData, cls=JSONEncoder)
+        return json.dumps(mikrotikData, cls=JSONEncoder)
+    except ServerSelectionTimeoutError:
+        error_message = {
+            "error": "connection_error",
+            "message": "Unable to connect to the MongoDB server."
+        }
+        # HTTP status code 500 indicates Internal Server Error
+        return jsonify(error_message), 500
 
 
 @mikrotik_bp.route('/addMikrotik', methods=['POST'])
